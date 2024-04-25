422079780ae4ab32f907f6fb183e44f2ab15b573
Jan Louis R. Rodriguez <113285150+riicosav@users.noreply.github.com>
2024-04-25 16:50:49 +0800

    kantutan

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center">SeatFreak</h1>
+      <div className="container">
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+      </div>
     </div>
+    //hello
   );
 }
 
 export default App;
+
+
