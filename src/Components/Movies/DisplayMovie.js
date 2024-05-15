e454e6de66b57799ca62a0e286f67b245669c03d
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-05-15 11:22:31 +0800

    List movies fixes

--- src/Components/Movies/DisplayMovie.js
+++ src/Components/Movies/DisplayMovie.js
@@ -1,46 +1,48 @@
-function DisplayMovie({movieProps, bookSeats, deleteMovie2, switchClick}) {
-    const {selectedMovies, setSelectedMovies, error} = movieProps
-    // Sorts and add movies by date
-    const moviesByDate = selectedMovies.reduce((acc, movie) => {
-        // If the date group doesn't exist, create it
-        if (!acc[movie.day]) {
-        acc[movie.day] = [];
-        }
-        // Add the movie to the corresponding date group
-        acc[movie.day].push(movie);
-        return acc;
-    }, {});
-
-    // Sort the days of the week
-    const sortedDays = Object.keys(moviesByDate).sort((a, b) => {
-        if (a === 'Monday') return -1; // Monday comes first
-        if (b === 'Monday') return 1;
-        return a.localeCompare(b); // Sort other days alphabetically
-    });
-
-    // Sort the time slots within each day
-    const sortedMoviesByDate = {};
-    sortedDays.forEach(day => {
-        sortedMoviesByDate[day] = moviesByDate[day].sort((a, b) => {
-        // Extract the start time from the time slot string
-        const startTimeA = a.time.split('-')[0];
-        const startTimeB = b.time.split('-')[0];
-        // Compare the start times
-        return startTimeA.localeCompare(startTimeB);
-        });
-    });
-
-    // Remove movie function
-    async function removeMovie(indexToRemove, index2) {
-        const newSelectedMovies = selectedMovies.filter((_, index) => index !== indexToRemove);
-        setSelectedMovies(newSelectedMovies);
-
-        deleteMovie2(index2);
-        // Add the movie to the corresponding date group
-        // acc[movie.day].push(movie);
-        // return acc;
+function DisplayMovie({ movieProps, bookSeats, deleteMovie2, switchClick }) {
+  const { selectedMovies, setSelectedMovies, error } = movieProps;
+  // Sorts and add movies by date
+  const moviesByDate = selectedMovies.reduce((acc, movie) => {
+    // If the date group doesn't exist, create it
+    if (!acc[movie.day]) {
+      acc[movie.day] = [];
     }
-    
+    // Add the movie to the corresponding date group
+    acc[movie.day].push(movie);
+    return acc;
+  }, {});
+
+  // Sort the days of the week
+  const sortedDays = Object.keys(moviesByDate).sort((a, b) => {
+    if (a === "Monday") return -1; // Monday comes first
+    if (b === "Monday") return 1;
+    return a.localeCompare(b); // Sort other days alphabetically
+  });
+
+  // Sort the time slots within each day
+  const sortedMoviesByDate = {};
+  sortedDays.forEach((day) => {
+    sortedMoviesByDate[day] = moviesByDate[day].sort((a, b) => {
+      // Extract the start time from the time slot string
+      const startTimeA = a.time.split("-")[0];
+      const startTimeB = b.time.split("-")[0];
+      // Compare the start times
+      return startTimeA.localeCompare(startTimeB);
+    });
+  });
+
+  // Remove movie function
+  async function removeMovie(indexToRemove, index2) {
+    const newSelectedMovies = selectedMovies.filter(
+      (_, index) => index !== indexToRemove
+    );
+    setSelectedMovies(newSelectedMovies);
+
+    deleteMovie2(index2);
+    // Add the movie to the corresponding date group
+    // acc[movie.day].push(movie);
+    // return acc;
+  }
+
   return (
     <div>
       {/* List of Added Movies */}
@@ -55,38 +57,39 @@ function DisplayMovie({movieProps, bookSeats, deleteMovie2, switchClick}) {
           <h2>
             <center>List of Movies:</center>
           </h2>
+
           {sortedDays.map((day) => (
             <div key={day}>
               {sortedMoviesByDate[day].map((movie, index) => (
                 <div key={index} className="movie-card">
-                  <div className="row">
-                    <div className="col">
+                  <div className="row no-gutters">
+                    <div className="gradient-img col-md-4">
                       <img
-                        className="dp-img"
+                        className="img-fluid dp-img"
                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                         alt={movie.title}
                       />
-                      <br />
                     </div>
-                    <div className="col">
-                      <h3> {movie.title}</h3>
-                      <p>Day: {movie.day}</p>
-                      <p>Time: {movie.time}</p>
-                      <br />
-                      <div className="dmButtons">
-                        <button
-                          type="button"
-                          className="btn btn-custom-primary"
-                          onClick={()=>bookSeats(movie.index)}
-                        >
-                          Book Seats
-                        </button>
-                        <button
-                          class="btn btn-custom-danger"
-                          onClick={() => removeMovie(index, movie.index)}
-                        >
-                          Remove
-                        </button>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h3 className="card-title">{movie.title}</h3>
+                        <p className="card-text">Day: {movie.day}</p>
+                        <p className="card-text">Time: {movie.time}</p>
+                        <div className="dmButtons mt-3">
+                          <button
+                            type="button"
+                            className="btn btn-custom-primary mr-2"
+                            onClick={() => bookSeats(movie.index)}
+                          >
+                            Book Seats
+                          </button>
+                          <button
+                            className="btn btn-custom-danger"
+                            onClick={() => removeMovie(index, movie.index)}
+                          >
+                            Remove
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
