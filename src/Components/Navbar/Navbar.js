7c8a5e320f23c114d3ce7e353edb4a4a6e31d68d
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-05-15 09:33:25 +0800

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import seatFreakLogo from "../../images/seatfreak3.png";
 import jans from "../../images/jans.png";
+
 function Navbar({ setDatesAppear, setSeatingAppear, setHomeAppear }) {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -32,12 +33,12 @@ function Navbar({ setDatesAppear, setSeatingAppear, setHomeAppear }) {
           Seats
         </button>
       </div>
-      {/* <a href="#" className="icon" onClick={toggleMenu}>
+      <a href="#" className="icon" onClick={toggleMenu}>
         <FontAwesomeIcon
           icon={menuOpen ? faXmark : faBars}
           className="menu-icon"
         />
-      </a> */}
+      </a>
     </div>
   );
 }
